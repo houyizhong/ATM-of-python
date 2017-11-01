abded45f20f61273ec8ee37c3d40e214e3d4574c
houyizhong <hou137010116@gmail.com>
2017-11-01 17:32:25 +0800

    What's new:a balance module demo about atm-of-python

---
+++ balance_module.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+#owner:houyizhong
+#version:1.1
+
+import pickle
+import os
+
+filename=os.path.abspath(__file__)
+dirname=os.path.dirname(filename)
+info_path=dirname+os.sep+'user.info'
+
+def write_balance(data):
+	f=open(info_path,'wb')
+	pickle.dump(data,f)
+	f.close()
